3322bddb8314f434e1b744f5f1c7954a0fb36b92
Hyunjoon Ko <linus@vlending.co.kr>
2024-08-07 18:09:16 +0900

    SPM manifest 업데이트.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,23 +8,15 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
-
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AppLink",
-            targets: ["AppLink"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "applink-ios-sdk",
+            targets: ["applink-ios-sdk"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AppLink",
-            dependencies: [],
-            path: "Framework"),
+        .binaryTarget(
+            name: "applink-ios-sdk",
+            path: "Framework/AppLink.xcframework"),
     ]
 )
